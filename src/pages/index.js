f9dea3b9ba778a5c3c1d0ae2de2a97a9f37e9cd6
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-08-29 10:18:59 -0500

    Image link.

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1 className="text-center">Enjoy NASA's Image of the Day</h1>
     <br />
-    <img src="https://apod.nasa.gov/apod/image/2008/PIA22089OrionValley.jpg" />
+    <img src="https://apod.nasa.gov/apod/image/2008/marsHirise_ESP_035143_1325_1096.jpg" />
     <br />
     <br />
   </Layout>
